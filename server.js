1c6287413e0609388507a77a0822843e7808ffc4
YehezkielDarren <yehezkiel.darren@ti.ukdw.ac.id>
2025-06-21 21:39:30 +0700

    first commit

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
+    // GatewayIntentBits.MessageContent,
   ],
 });
 
